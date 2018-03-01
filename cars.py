488f0c3a830ccb970473282e227d79cf8b72e335
Jonah Joughin <jonah@jough.in>
2018-03-01 23:01:30 +0400

    Fixed base_value

--- cars.py
+++ cars.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 
 Point = namedtuple("Point", "x y")
+Car = namedtuple("Car", "x y free")
 Ride = namedtuple("Ride", "start latest end begin finish")
 
 def distance(p1, p2):
@@ -15,37 +16,49 @@ def ride(line):
   latest = finish - distance(start, end)
   return Ride(start, latest, end, begin, finish)
 
-def closeEnough(car, ride, current, thresh):
+def closeEnough(car, ride, current, m_thresh, a_thresh):
 
   pickup_distance = distance(car, ride.start)
   trip_distance = distance(ride.start, ride.end)
   if (ride.latest < current + pickup_distance):
     return False
-  if (trip_distance < pickup_distance * thresh):
+  if (trip_distance < pickup_distance * m_thresh):
     return False
-  if (pickup_distance * thresh < (ride.begin - current)):
+  if (pickup_distance * m_thresh + a_thresh < (ride.begin - current)):
     return False
   return True
 
-def candidateRides(car, rides, current, thresh):
-  return [ride for ride in rides if closeEnough(car, ride, current, thresh)]
+def candidateRides(car, rides, current, m_thresh, a_thresh):
+  crs = [ride for ride in rides if closeEnough(car, ride, current, m_thresh, a_thresh)]
+  if len(crs) < 2:
+     return candidateRides(car, rides, current, m_thresh*2, a_thresh*2)
+  else:
+    return crs
 
-def base_cost(car, ride, current):
+# Simple value for a given ride, car, and on_time_points
+# Higher is better
+
+def base_value(car, ride, current):
   if (ride.begin < current):
     return None
-  trip_distance = abs(ride.end.x - ride.start.x) + abs(ride.end.y - ride.start.y)
-  pickup_distance = abs(car.x - ride.start.x) + abs(car.y - ride.start.y)
+  trip_distance = distance(ride.start, ride.end)
+  pickup_distance = distance(ride.start, car)
   pickup_time = ride.begin - current
   elapsed = max(pickup_distance, pickup_time) + trip_distance
   pickup_points = B if pickup_distance <= pickup_time else 0
-  on_time_points = elapsed if (current + elapsed) < ride.finish else 0
+  on_time_points = trip_distance if (current + elapsed) < ride.finish else 0
   return (pickup_points + on_time_points) / elapsed
 
-lines = [line.rstrip("\n") for line in open("a_example.in")]
+lines = [line.rstrip("\n") for line in open("b_should_be_easy.in")]
 R, C, F, N, B, T = [int(x) for x in lines[0].split(" ")]
 rides = [ride(line) for line in lines[1:]]
+cars = [Car(0, 0, 0) for x in range(F)]
+time = 0
 
-cars = [Point(0,0) for x in range(F)]
-cr = candidateRides(cars[0], rides, 0, 1)
-print(cr)
+cr = candidateRides(cars[0], rides, 0, 1, 2)
+bestRide = max(cr, key=lambda x: base_value(cars[0], x, time))
+
+print([base_value(cars[0], ride, time) for ride in rides])
+print([base_value(cars[0], ride, time) for ride in cr])
+print(base_value(cars[0], bestRide, time))
 #print(rides)
